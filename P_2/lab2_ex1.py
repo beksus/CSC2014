ed8f8e0ab7fb55fdfb98dbd1993371163d4f541a
Beks <85835904+beksus@users.noreply.github.com>
2025-05-14 00:43:48 +0000

    ex01

---
+++ P_2/lab2_ex1.py
@@ -0,0 +1,34 @@
+import numpyimport numpy as np
+
+# 4x4 array
+array_4x4 = np.array([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+])
+
+# 5x4 array
+array_5x4 = np.array([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+    [17, 18, 19, 20]
+])
+
+# 4x1 array
+array_4x1 = np.array([
+    [1],
+    [2],
+    [3],
+    [4]
+])
+
+# Print arrays to verify
+print("4x4 array:")
+print(array_4x4)
+print("\n5x4 array:")
+print(array_5x4)
+print("\n4x1 array:")
+print(array_4x1)

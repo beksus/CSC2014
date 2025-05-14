ee82014474a6863865534c1cd6f8a6df5cbebbcd
Beks <85835904+beksus@users.noreply.github.com>
2025-05-14 00:40:02 +0000

    added this useless shit

---
+++ P_2/array_manipulation.py
@@ -0,0 +1,8 @@
+array1D_int = numpy.array([[1,2]])                                  #Create a 1x2 array
+array2D_int = numpy.array([[1,2,3],[4,5,6],[7,8,9]])                #Create a 3x3 array
+array1D_float = numpy.array([[1.2,3.4,5.6]])                        #Create a 1x3 array
+
+
+print (array1D_int)
+print (array2D_int)
+print (array1D_float)

c2450f50484ad0c1ed3a750cf506476b949ad0e5
Beks <85835904+beksus@users.noreply.github.com>
2025-06-11 01:01:02 +0000

    added ex02

--- P_6/ex02.py
+++ P_6/ex02.py
@@ -6,94 +6,23 @@ Created on Wed Jun 11 08:46:45 2025
 """
 
 import numpy as np
-
-
-
 import cv2
-
-
-
-
-
 from matplotlib import pyplot as pt
 
-
-
-
-
-simple_image = np.zeros ((256,256), dtype=np. float64)
-
-
-
-simple_image[124:132,124:132] = 255
-
-
-
-
-
-fourier_coeff = np.fft. fft2(simple _image)
-
-
-
-
+simple_image = np.zeros((256, 256), dtype=np.float64)
+simple_image[124:132, 124:132] = 255
+fourier_coeff = np.fft.fft2(simple_image)
 fourier_coeff_center = np.fft.fftshift(fourier_coeff)
-
-
-
 spectrum = np.abs(fourier_coeff_center)
-
-
-
-
-pt. figure()
-spectrum_10g = np. 10g(1+spectrum)
-
-
-
-
-
-
-
-
-pt.subplot (2,2,1)
-
-
-
-
-
-pt. amshow (simple_image, cmap="gray")
-
-
-
-
-pt. title "Simple Image")
-
-
-
-pt. subplot (2,2,2)
-
-
-
-
-
-pt. imshow(spectrum, cmap="gray")
-
-
-
-
-pt. title("Spectrum Before Log Enhanced")
-
-
-
-pt. subplot (2,2,3)
-
-
-
-
-
-pt. imshow(spectrum_1og, cmap="gray")
-
-
-
-
-pt. title("Spectrum After Log Enhanced")
+pt.figure()
+spectrum_log = np.log(1 + spectrum)
+pt.subplot(2, 2, 1)
+pt.imshow(simple_image, cmap="gray")
+pt.title("Simple Image")
+pt.subplot(2, 2, 2)
+pt.imshow(spectrum, cmap="gray")
+pt.title("Spectrum Before Log Enhanced")
+pt.subplot(2, 2, 3)
+pt.imshow(spectrum_log, cmap="gray")
+pt.title("Spectrum After Log Enhanced")
+# ...existing code...
